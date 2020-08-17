a8f425e555e647bb8baa5b4da1ee4aed9e2680ec
Mark Goldthorp <code@koalatea-software.com>
2020-08-17 15:41:50 +0100

    phonegap platform remove android - in the hope that this will fix the error " Cannot find module 'compare-func' "

--- platforms/android/app/src/main/assets/www/js/backButtonHandler.js
+++
@@ -1,8 +0,0 @@
-// todo: a the moment, it only goes back the sentinels list. It has to handle different stone lists, and even the stone classes
-/**
- * This wil lbe of use (at the moment) in taking you back from viewing a specific stone, to a list of stones
- */
-document.getElementById("logo").onclick = function () {
-        specificStoneDisplayHandler.hide();
-        StoneSetClass.show("sentinel");
-    }
